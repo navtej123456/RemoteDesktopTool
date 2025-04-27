6bbbb4311b475241b6d75c9a27089ea8528bf732
navtej123456 <navtejsghh5050@gmail.com>
2025-04-27 12:34:36 +0530

    Update AI_main.py

--- AI_main.py
+++ AI_main.py
@@ -1,10 +1,1 @@
-from transformers import pipeline
 
-chatbot = pipeline("text-generation", model="gpt2")
-
-while True:
-    user_input = input("You: ")
-    if user_input.lower() == "exit":
-        break
-    response = chatbot(user_input, max_length=50, do_sample=True, temperature=0.7)
-    print("Bot:", response[0]['generated_text'])
